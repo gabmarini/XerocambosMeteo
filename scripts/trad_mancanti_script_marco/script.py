2eba66ce030ef848503f6f659e2af16617286a13
gabmarini <gab.marini2@gmail.com>
2017-03-02 17:48:53 +0100

    trans added

--- scripts/trad_mancanti_script_marco/script.py
+++ scripts/trad_mancanti_script_marco/script.py
@@ -1,8 +1,11 @@
+# -- coding: latin-1 --
 import csv
+import sys
+import codecs
 
-with open('traduzioni_mancanti.txt', "rb") as file:
-	reader = csv.reader(file, delimiter="|")
-	language = "si"
+with open("traduzioni_mancanti.txt", "rb") as inputFile:
+	reader = csv.reader(inputFile, delimiter="|")
+	language = sys.argv[1]
 	for row in reader: 
 		if(row[1] == language):
-			print 'langlookup|' + row[0].strip(' ') + '|' + row[2].strip(' ') + '|'
+			print 'langlookup|' + row[0].strip(' ').decode('utf-8').encode('ISO-8859-7') + '|' + row[2].strip(' ').decode('utf-8').encode('ISO-8859-7') + '|'
